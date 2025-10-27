2f7a84fd3e49c304c6fe98b298ff66ee57e76fda
Rick Baier <you@example.com>
2025-10-26 23:33:49 -0500

    initial commit on develop

---
+++ alien_invasion.py
@@ -0,0 +1,34 @@
+import sys
+import pygame
+from settings import Settings
+
+class AlienInvasion:
+    #Overall class to manage game assets and behavior
+    def __init__(self):
+        #Initialize the game, and create game resources
+        pygame.init()
+        self.clock = pygame.time.Clock() # Set frame rate to 60 FPS
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+
+
+    def run_game(self):
+        #Start the main loop for the game
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            #Redraw the screen during each pass through the
+            self.screen.fill(self.settings.bg_color)
+            
+            #Make the most recently drawn game visible
+            pygame.display.flip()
+            self.clock.tick(60)
+
+if __name__ == '__main__':
+    #Make a game instance, and run the game
+    ai = AlienInvasion()
+    ai.run_game()
