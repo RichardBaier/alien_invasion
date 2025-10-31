e5dd60b849dce76789531edef819f93e753c5557
Rick Baier <you@example.com>
2025-10-30 22:14:00 -0500

    Added collision for bullets, ship, and aliens reaching the bottom of the screen. THe ship also has 3 lives.

--- settings.py
+++ settings.py
@@ -9,13 +9,14 @@ class Settings:
 
         # Ship settings
         self.ship_speed = 1.5
+        self.ship_limit = 3
 
         # Bullet Settings
-        self.bullet_speed = 2.0
+        self.bullet_speed = 2.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
-        self.bullets_allowed = 3
+        self.bullets_allowed = 5
 
         # Alien settings
         self.alien_speed = 1.0
--- ship.py
+++ ship.py
@@ -2,16 +2,16 @@ import pygame
 
 class Ship:
     def __init__(self, ai_game):
-        #Initialize the ship and set its starting position
+        # Initialize the ship and set its starting position
         self.screen = ai_game.screen
         self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
-        #Load the ship image and get its rect
+        # Load the ship image and get its rect
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
 
-        #Start each new ship at the bottom center of the screen
+        # Start each new ship at the bottom center of the screen
         self.rect.midbottom = self.screen_rect.midbottom
 
         # Store a float for the ship's horizontal position
@@ -21,9 +21,14 @@ class Ship:
         self.moving_right = False
         self.moving_left = False
 
+    def center_ship(self):
+        # Center the ship on the screen
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
+
     def update(self):
-        #Update the ship's position based on movement flags
-        #Update the ship's x value, not the rect
+        # Update the ship's position based on movement flags
+        # Update the ship's x value, not the rect
         if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
         if self.moving_left and self.rect.left > 0:
@@ -33,5 +38,5 @@ class Ship:
         self.rect.x = self.x
 
     def blitme(self):
-        #Draw the ship at its current location
+        # Draw the ship at its current location
         self.screen.blit(self.image, self.rect)
